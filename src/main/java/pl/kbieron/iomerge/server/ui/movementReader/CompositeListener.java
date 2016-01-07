238538f0e25371e34ea941c901c2b7dbddddeccf
krzychek <krzysiek1993r@gmail.com>
2016-01-07 23:20:38 +0100

    Inline CompositeListener

--- src/main/java/pl/kbieron/iomerge/server/ui/movementReader/CompositeListener.java
+++ src/main/java/pl/kbieron/iomerge/server/ui/movementReader/CompositeListener.java
@@ -1,21 +1,70 @@
 package pl.kbieron.iomerge.server.ui.movementReader;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.kbieron.iomerge.server.gesture.GestureRecorder;
 import pl.kbieron.iomerge.server.utilities.MovementListener;
 
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
+import static java.awt.event.MouseEvent.BUTTON3;
 
-public interface CompositeListener extends MovementListener, MouseListener, MouseWheelListener, KeyListener {
+
+class CompositeListener implements MovementListener, MouseListener, MouseWheelListener, KeyListener {
+
+	private MovementListener movementListener;
+
+	@Autowired
+	private VirtualScreen virtualScreen;
+
+	@Autowired
+	private GestureRecorder gestureRecorder;
 
 	@Override
-	default void mouseClicked(MouseEvent mouseEvent) {}
+	public void mousePressed(MouseEvent mouseEvent) {
+		if ( mouseEvent.getButton() == BUTTON3 ) {
+			movementListener = gestureRecorder;
+		}
+		movementListener.mousePressed();
+	}
 
 	@Override
-	default void mouseEntered(MouseEvent mouseEvent) {}
+	public void mouseReleased(MouseEvent mouseEvent) {
+		movementListener.mouseReleased();
+		if ( mouseEvent.getButton() == BUTTON3 ) {
+			movementListener = virtualScreen;
+		}
+	}
 
 	@Override
-	default void mouseExited(MouseEvent mouseEvent) {}
+	public void mouseWheelMoved(MouseWheelEvent mouseWheelEvent) {
+		movementListener.mouseWheelMoved(mouseWheelEvent.getWheelRotation());
+	}
+
+	@Override
+	public void move(int dx, int dy) {
+		movementListener.move(dx, dy);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent keyEvent) {virtualScreen.keyTyped(keyEvent);}
+
+	@Override
+	public void keyPressed(KeyEvent keyEvent) {virtualScreen.keyPressed(keyEvent);}
+
+	@Override
+	public void keyReleased(KeyEvent keyEvent) {virtualScreen.keyReleased(keyEvent);}
+
+	@Override
+	public void mouseClicked(MouseEvent mouseEvent) {}
+
+	@Override
+	public void mouseEntered(MouseEvent mouseEvent) {}
+
+	@Override
+	public void mouseExited(MouseEvent mouseEvent) {}
 }
