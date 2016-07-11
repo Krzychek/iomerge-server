a77ccf40bd664d272d1811da094bb23a4b0b82c1
krzychek <krzysiek1993r@gmail.com>
2016-07-11 20:16:39 +0200

    upgrade annoprops

--- src/main/java/pl/kbieron/iomerge/server/Bootstrap.java
+++ src/main/java/pl/kbieron/iomerge/server/Bootstrap.java
@@ -1,19 +1,16 @@
 package pl.kbieron.iomerge.server;
 
-import org.annoprops.PropertyManager;
+import org.annoprops.PropertyManagerHelperBean;
+import org.annoprops.springframework.SpringframeworkAnnopropsBeanFactory;
 import org.kohsuke.args4j.CmdLineException;
-import org.pmw.tinylog.Logger;
 import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
-import org.springframework.stereotype.Component;
 import pl.kbieron.iomerge.server.config.TinyLogConfigurator;
 
-import javax.annotation.PreDestroy;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.io.IOException;
@@ -39,32 +36,7 @@ public class Bootstrap {
 	}
 
 	@Bean
-	PropertyManager propertyManager(ListableBeanFactory beanFactory) throws IOException {
-		PropertyManager propertyManager = PropertyManager.builder() //
-				.withDefaultSerializers() //
-				.withSpringListableBeanFactory(beanFactory) //
-				.build();
-
-
-		if (SETTINGS_FILE.exists()) {
-			propertyManager.readPropertiesFromFile(SETTINGS_FILE);
-		} else {
-			Logger.info("File:" + SETTINGS_FILE + " does not exits, probably first run?");
-		}
-
-		return propertyManager;
-	}
-
-	@Component
-	private static class ContextLifecycleWrapper {
-
-		@Autowired
-		private PropertyManager propertyManager;
-
-		@PreDestroy
-		private void shutdown() throws IOException {
-			Logger.info("shutting down");
-			propertyManager.savePropertiesToFile(SETTINGS_FILE);
-		}
+	PropertyManagerHelperBean propertyManager(ListableBeanFactory beanFactory) throws IOException {
+		return SpringframeworkAnnopropsBeanFactory.createWithSpringFactory(beanFactory, SETTINGS_FILE);
 	}
 }
