8b114bf2f3d8f4568c6a8088f16801307fd6a8c8
Krzychek <krzychek>
2016-07-14 21:39:38 +0200

    export gesture plugin to separate repo

--- gesture-plugin/src/main/java/pl/kbieron/iomerge/plugins/server/gesture/PluginConfiguration.java
+++
@@ -1,9 +0,0 @@
-package pl.kbieron.iomerge.plugins.server.gesture;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-
-@Configuration
-@ComponentScan("pl.kbieron.iomerge.plugins.server.gesture")
-public class PluginConfiguration {}
