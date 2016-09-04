f2ca1e4d57123e430091d1ab39ec11df7f12a01c
Krzychek <krzychek>
2016-09-04 18:12:45 +0200

    move clipboard staff to one file

--- iomerge-server-app/src/main/java/com/github/krzychek/iomerge/server/misc/ClipboardUtils.kt
+++ iomerge-server-app/src/main/java/com/github/krzychek/iomerge/server/misc/ClipboardUtils.kt
@@ -1,20 +1,25 @@
 package com.github.krzychek.iomerge.server.misc
 
-
 import com.github.krzychek.iomerge.server.api.appState.AppState
 import com.github.krzychek.iomerge.server.api.network.MessageDispatcher
 import com.google.common.eventbus.Subscribe
 import org.pmw.tinylog.Logger
 import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.StringSelection
 import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
-/**
- * Manages system clipboard, syncs clipboard on remote entering
- */
+@Singleton class ClipboardContentSetter
+@Inject constructor(private val systemClipboard: Clipboard) {
+
+	fun setClipboardContent(data: String) {
+		systemClipboard.setContents(StringSelection(data), null)
+	}
+}
+
 @Singleton class ClipboardSynchronizer
 @Inject constructor(private val messageDispatcher: MessageDispatcher, private val clipboard: Clipboard) {
 
