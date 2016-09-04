300868dd52be696ceea2f437687047fefc5fc935
Krzychek <krzychek>
2016-09-04 18:11:39 +0200

    unimplement unused iface

--- iomerge-server-app/src/main/java/com/github/krzychek/iomerge/server/misc/ClipboardContentSetter.kt
+++ iomerge-server-app/src/main/java/com/github/krzychek/iomerge/server/misc/ClipboardContentSetter.kt
@@ -1,21 +1,15 @@
 package com.github.krzychek.iomerge.server.misc
 
-
 import java.awt.datatransfer.Clipboard
-import java.awt.datatransfer.ClipboardOwner
 import java.awt.datatransfer.StringSelection
-import java.awt.datatransfer.Transferable
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton class ClipboardContentSetter
-@Inject constructor(private val systemClipboard: Clipboard) : ClipboardOwner {
+@Inject constructor(private val systemClipboard: Clipboard) {
 
 	fun setClipboardContent(data: String) {
-		systemClipboard.setContents(StringSelection(data), this)
-	}
-
-	override fun lostOwnership(clipboard: Clipboard, contents: Transferable) {
+		systemClipboard.setContents(StringSelection(data), null)
 	}
 }
