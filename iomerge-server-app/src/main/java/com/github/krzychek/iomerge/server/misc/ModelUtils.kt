078043c20652d5248b0e42ef8d1451bb7ddd9c11
Krzychek <krzychek>
2016-09-26 17:05:13 +0200

    fix mouse button mappings

--- iomerge-server-app/src/main/java/com/github/krzychek/iomerge/server/misc/ModelUtils.kt
+++ iomerge-server-app/src/main/java/com/github/krzychek/iomerge/server/misc/ModelUtils.kt
@@ -10,8 +10,8 @@ val MouseButton.awtButton: Int get() = when (this) {
 	MouseButton.RIGHT -> InputEvent.BUTTON3_DOWN_MASK
 }
 val MouseEvent.mouseButton: MouseButton get() = when (this.button) {
-	InputEvent.BUTTON1_DOWN_MASK -> MouseButton.LEFT
-	InputEvent.BUTTON2_DOWN_MASK -> MouseButton.CENTER
-	InputEvent.BUTTON3_DOWN_MASK -> MouseButton.RIGHT
-	else -> throw IllegalStateException("wrong awt button")
+	MouseEvent.BUTTON1 -> MouseButton.LEFT
+	MouseEvent.BUTTON2 -> MouseButton.CENTER
+	MouseEvent.BUTTON3 -> MouseButton.RIGHT
+	else -> throw IllegalStateException("wrong awt button ${this.button}")
 }
